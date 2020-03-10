f255883744fc7fa43094d5ff8c6112b492ac844d
Douglas de Souza <douglas.souzag@hotmail.com>
2020-03-10 08:55:11 -0300

    added - agroApi
     - Adicionado método novo para baixar imagens de satélite
    - Novo utilitário para baixar imagem de talhão
    - Adicionado pasta ./treinamento no gitignore
    - Adicionado rotas de criar,remover áreas na API principal.

--- server/app.py
+++ server/app.py
@@ -1,48 +1,63 @@
-from flask import Flask, request, jsonify
+# coding: utf-8
+
+from flask import Flask, request, jsonify, Blueprint
 from flask_cors import CORS
 from flask_restplus import Api, Resource, fields
 from apiCopernicus import baixarArea
+from apiAgro import Poligono
 import json
 
 
 app = Flask(__name__)
+app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
+blueprint = Blueprint('api', __name__, url_prefix='/api')
 CORS(app)
-api = Api(app)
+api = Api(
+    blueprint,
+    doc='/doc/',
+    version = "1.0", 
+	title = "Visiona", 
+	description = "Descrição aqui"
+)
+app.register_blueprint(blueprint)
 
-model_geojson = api.model('Area',{'area': fields.String(required = True)})
-model_data = api.model('Data',{'data': fields.Date(required = True)})
-sentinel = api.namespace('sentinel', description='Principais APIs')
+sentinel = api.namespace('sentinel', description='Rotas principais')
+area = api.namespace('area', description='Áreas')
 
 
-@sentinel.route('/teste', methods=['GET'])
-class Teste(Resource):
+@sentinel.route('/status', methods=['GET'])
+class Status(Resource):
     def get(self):
         return {'sucesso':'Api funcionando!'},200
 
-        
-@sentinel.route('/processar', methods=['POST'])
-class BaixarArea(Resource):
-    @sentinel.expect(model_geojson)
+@area.route('/criar')
+class criarArea(Resource):
+    @area.expect(
+        api.model('GeoJSON',{'geo_json': fields.Raw(required = True),'name': fields.String(required = True)})
+    )
     def post(self):
-        area = sentinel.payload['area']
+        geo = area.payload['geo_json']['geo_json']['geometry']['coordinates']
+        nome = area.payload['name']
+
+        poligono = Poligono(geo,nome,'')
+        poligono.criar()
+        return {'id':poligono.id}
+
+@area.route('/excluir')
+class deletarArea(Resource):
+    @area.expect(
+        api.model('Area',{'id': fields.String(required = True)})
+    )
+    def delete(self):
+        id = area.payload['id']
 
         try:
-            with open('area.json', 'w', encoding='utf-8') as arquivo:
-                json.dump(json.loads(area), arquivo, ensure_ascii=False, indent=4)
-            arquivo.close()
+            poligono = Poligono('','',id)
+            poligono.carregar()
+            poligono.deletar()
+            return 'Área excluida!',200
         except:
-            return {"erro":"Erro ao gerar geojson da área."}
-
-        try:
-            baixarArea()
-        except:
-            return {"erro":"Erro ao baixar área demarcada."}
-
-        
-        return {'sucesso':'teste'},200
-
-
-
+            return 'Área não existe!',200
 
 if __name__ == '__main__':
     app.config['JSON_AS_ASCII'] = False
