4f4755e4a12e67ac1fd1108daf874c743f4d9fc3
Douglas de Souza <douglas.souzag@hotmail.com>
2020-05-19 19:30:08 -0300

    Feature - Componente de toolbar criado

---
+++ client/src/components/Navbar/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <v-toolbar dark flat app>
+
+      <v-app-bar-nav-icon @click="navbar = !navbar"></v-app-bar-nav-icon>      
+      <v-toolbar-title>Visiona</v-toolbar-title>
+    </v-toolbar>
+
+    <v-navigation-drawer app dark v-model="navbar">
+      <p>TEste</p>
+    </v-navigation-drawer>
+
+
+  </nav>
+</template>
+
+<script src="./NavbarController.js"></script>
