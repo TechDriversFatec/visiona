0513ba9d60557536c5d32535a9770b4d13014623
Henrique Alexandre <henrique@flipfloplab.com.br>
2020-05-28 07:51:18 -0300

    Etapa visual da tela de áreas.

--- client/src/views/Areas/Areas.vue
+++ client/src/views/Areas/Areas.vue
@@ -1,7 +1,74 @@
 <template>
-    <p>Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Atirei o pau no gatis, per gatis num morreus. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Si num tem leite então bota uma pinga aí cumpadi!
-    Diuretics paradis num copo é motivis de denguis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Casamentiss faiz malandris se pirulitá.
-    A ordem dos tratores não altera o pão duris. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Per aumento de cachacis, eu reclamis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.</p>
+
+  <v-container grid-list-xs>
+  <v-card
+    flat
+    tile
+    class="mx-auto transparent"
+  >
+    <v-container class="pa-1">
+      <v-item-group
+        multiple
+        column
+      >
+        <v-row>
+          <v-col
+            v-for="item in arrayEpisodios"
+            :key="item.episodio"
+            cols="4"
+            md="3"
+          >
+            <v-item v-if="item.episodio>0 && item.episodio <=12">
+              <v-hover v-slot:default="{ hover }">
+                <v-card
+                  outlined
+                  height="250"
+                  max-width="344"
+                  class="mx-auto black"
+                  :elevation="hover ? 12 : 2"
+                >
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title class="white--text headline">AREA {{formatarNum(item.episodio)}}</v-list-item-title>
+                      <v-list-item-subtitle class="grey--text">dd/mm/aaaa</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+
+                  <v-img
+                    src="..\..\assets\Imagens_Remover\mapaicon.png"
+                    max-height="120"
+                    class="pa-7"
+                  ></v-img>
+
+                  <v-card-actions>
+                    <Card></Card>
+                    <v-btn class="white--text" icon>
+                      <v-icon>mdi-flash</v-icon>
+                    </v-btn>
+
+                  </v-card-actions>
+                </v-card>
+              </v-hover>
+            </v-item>
+          </v-col>
+        </v-row>
+      </v-item-group>
+    </v-container>
+    </v-card>
+
+  </v-container>
 </template>
+
+<script src="./PainelController.js"></script>
+
+<style scoped>
+
+.transparent {
+   background-color: white!important;
+   opacity: 0.65;
+   border-color: transparent!important;
+ }
+</style>
 
 <script src="./AreasController.js"></script>
--- client/src/views/Areas/AreasController.js
+++ client/src/views/Areas/AreasController.js
@@ -1,3 +1,37 @@
+import Episodios from '../../service/episodios'
+import Card from '../../components/Card/Card.vue'
+// import Episodio from '../../components/Episodio/Episodio.vue'
+
 export default {
-    
-}
+    components: {
+      Card,
+
+    },
+    data () {
+      return {
+        arrayEpisodios: [],
+        link : '',
+      }
+    },
+    mounted () {
+
+      if (localStorage.getItem('data')){
+        this.arrayEpisodios = JSON.parse(localStorage.getItem('data'));
+      } else {
+        Episodios.listar().then(resposta =>{
+          this.arrayEpisodios = resposta.data;
+          localStorage.setItem('data', JSON.stringify(resposta.data))
+        })
+      }
+    },
+    methods: {
+      formatarNum(num){
+        num = ("00" + num.toString()).slice(-3);
+        return num
+      },
+      chamarPlayer(num){
+        this.link = '/episodio/'+num.toString()
+        this.$router.push(this.link)
+      }
+    },
+  }
