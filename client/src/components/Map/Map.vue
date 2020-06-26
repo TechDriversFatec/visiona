fc56248769d1f774d7b3c0f81de62aa7ae48c07f
gsilva14 <gabriel.silvam@sonda.com>
2020-06-25 22:53:23 -0300

    adicionando parametros de busca

--- client/src/components/Map/Map.vue
+++ client/src/components/Map/Map.vue
@@ -4,7 +4,7 @@
     <div id="mapbox" class="mapbox"></div>
 
     
-    <v-card align="center" width="24%" class="white--text" style="background-color: #000411;">
+    <v-card align="center" width="24%" class="white--text" style="background-color: #7685a7">
       <v-card-title>Informações</v-card-title>
       <v-card-text>
         <v-form v-model="form">
@@ -22,7 +22,7 @@
             :value="JSON.stringify(poligono.geojson)"
             auto-grow
             outlined
-            row-height="25"
+            row-height="5"
           ></v-textarea>
           <v-text-field
             background-color="#e1efe6"
@@ -41,6 +41,49 @@
             :rules = "[rules.area_ha]"
             outlined
           ></v-text-field>
+          <v-col cols="12" class="pb-0 pt-0">
+        <v-subheader class="pl-4 white--text">Porcentagem de Nuvem:</v-subheader>
+        <v-slider
+          v-model="slider"
+          thumb-label
+        ></v-slider>
+      </v-col>
+       <v-col cols="12" class="pt-0">
+          <v-subheader class="pl-4 white--text">Satélites:</v-subheader>
+          <v-radio-group class="pt-0">
+            <v-radio value="sentinel" label="Sentinel"></v-radio>
+            <v-radio value="landsat" label="Landsat"></v-radio>
+          </v-radio-group>
+        </v-col>
+
+          <v-col class="pt-0 pb-0">
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="date"
+        transition="scale-transition"
+        offset-y
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="date"
+            label="Select Date"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="date" no-title scrollable>
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+          <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+        </v-date-picker>
+      </v-menu>
+    </v-col>
+
+ 
         </v-form>
         </v-card-text>
       <v-btn
@@ -54,12 +97,22 @@
     </v-row>
   </div>
 </template>
-<script src="./MapController.js"></script>
+<script src="./MapController.js">
+
+  export default {
+    data () {
+      return {
+        picker: new Date().toISOString().substr(0, 10),
+      }
+    },
+  }
+
+</script>
 
 <style scoped>
   .mapbox{
     width: 75%;
-    height: 750px;
+    height: 800px;
     border-radius: 5px;
   }
 </style>
