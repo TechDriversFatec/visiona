38d8b1d8dc84b51674121e444943af2bde5f6f73
Douglas <Souza>
2020-06-29 03:30:28 -0300

    added - servicos das areas

--- client/src/services/areas.js
+++ client/src/services/areas.js
@@ -17,5 +17,29 @@ export default{
             'x_access_token': store.state.accessToken
         }
         return http.post('areas',objetoArea,{headers})
-    }
+    },
+    baixarImagens:(id_area) => {
+
+        const data = {id: id_area}
+        const headers = {
+            'x_access_token': store.state.accessToken
+        }
+        return http.post('download',data,{headers})
+    },
+    processarImagens:(id_area) => {
+
+        const data = {id: id_area}
+        const headers = {
+            'x_access_token': store.state.accessToken
+        }
+        return http.post('process',data,{headers})
+    },
+    retornarThumbnail:(id_area) => {
+
+        const data = {area_id: id_area}
+        const headers = {
+            'x_access_token': store.state.accessToken
+        }
+        return http.post('thumbnail',data,{headers})
+    },
 }
