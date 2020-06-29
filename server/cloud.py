8b77000bf78fcb7cadf247010e2ef4cc38b46fd7
Enzo <enzo.gerola@fatec.sp.gov.br>
2020-06-28 23:38:15 -0300

    Auto stash before merge of "development" and "origin/development"
    # Adicionando função de upload da imagem na nuvem

---
+++ server/cloud.py
@@ -0,0 +1,16 @@
+import cloudinary
+import cloudinary.uploader
+
+cloudinary.config(
+    cloud_name='did33wj6e',
+    api_key='586894584935272',
+    api_secret='juv7Iw5NWW4VyjDf5vFB7VfJvrA')
+
+def uploadImg(file):
+    # file = request.files['picture']
+
+    upload_data = cloudinary.uploader.upload(file)
+    return {
+        'status': 1,
+        'data': upload_data.data.secure_url,
+    }
